d9d018b83d4034f9566feb43342105ba4eb83dcf
petrzjunior <junior@zahradnik.cz>
2020-03-15 14:23:46 +0100

    Add tests for error responses

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs';
 import test from 'ava';
+
 const { EETClient } = require('../src/EETClient');
 import * as crypto from '../src/crypto';
 import * as utils from '../src/utils';
@@ -9,6 +10,7 @@ import * as schema from '../src/schema';
 
 
 const PRIVATE_KEY = fs.readFileSync('./test/certificate-CZ1212121218/private.pem');
+const FAKE_PRIVATE_KEY = fs.readFileSync('./test/certificate-CZ1212121218/fake.pem');
 const CERTIFICATE = fs.readFileSync('./test/certificate-CZ1212121218/certificate.pem');
 const TEST_PKP = 'QEIvS/3ETSJuAK7agvrVlQUN1Oi4DoPrNBmC+sQueNknhsr48RGElLpzTnxH/KUdfde91xFOcRbgyiXapK4beRTRaZ/CQ1qug4Y7JbnhB60WUH61E2NlTzxTfmidcNIlQohrVDC5awyrZQj2T1cG+3gGPHQ/oveM4ozt5gLaHFDwl421eLQctxeQfXK4dDrZDANX6AVB8Q92X89o9YouISCjIrYk7ZnLhDe+cXxlB0GGJq5i1P2uALOgQyZBU5mBWLolL2n06C73Sja7HjCt9E8s6bV9y1cJZcjXo1tWOEUqfU8ir/wYstO11v/JmiRADGwGoCuCszktUmf4K3PaDg==';
 
@@ -44,21 +46,41 @@ test('convertAmountToString', t => {
 	t.is(utils.convertAmountToString(1200), '12.00');
 });
 
-test('parseRequest required', t => {
+test('parseRequest null', t => {
+	t.throws(() => schema.parseRequest(null));
+});
 
+test('parseRequest valid', t => {
 	t.notThrows(() => schema.parseRequest({
 		dicPopl: 'CZ1212121218',
+		dicPoverujiciho: 'CZ1212121218',
 		idPokl: '1',
 		idProvoz: 1,
 		poradCis: '2016-0001s',
 		datTrzby: new Date(),
 		celkTrzba: 1000,
 	}));
+});
 
+test('parseRequest wrong type', t => {
+	t.throws(() => schema.parseRequest({
+		dicPopl: 'CZ1212121218',
+		idPokl: '1',
+		idProvoz: 1,
+		poradCis: '2016-0001s',
+		datTrzby: new Date(),
+		celkTrzba: 'abc',
+	}));
+});
+
+test('parseRequest missing required', t => {
 	t.throws(() => schema.parseRequest({
 		idPokl: '1',
+		idProvoz: 1,
+		poradCis: '2016-0001s',
+		datTrzby: new Date(),
+		celkTrzba: 1000,
 	}));
-
 });
 
 test('validateVatId', t => {
@@ -116,7 +138,68 @@ test('parseRequest', t => {
 
 });
 
-test('do request', async t => {
+test('request correct', async t => {
+
+	const data = {
+		prvniZaslani: true,
+		overeni: false,
+		dicPopl: 'CZ1212121218',
+		idPokl: '/5546/RO24',
+		poradCis: '0/6460/ZQ42',
+		datTrzby: new Date(),
+		celkTrzba: 3411300,
+		idProvoz: 273,
+	};
+
+	const options = {
+		playground: true,
+		privateKey: PRIVATE_KEY,
+		certificate: CERTIFICATE,
+	};
+
+	const { response: { fik, warnings } } = await new EETClient(options).request(data);
+
+	t.not(fik, undefined);
+	t.is(fik.length, 39);
+	t.is(warnings, undefined);
+
+	t.log('FIK:', fik);
+
+});
+
+test('request wrong online', async t => {
+
+	const data = {
+		prvniZaslani: true,
+		overeni: false,
+		dicPopl: 'CZ1212121218',
+		idPokl: '/5546/RO24',
+		poradCis: '0/6460/ZQ42',
+		datTrzby: new Date('2016-08-05T00:30:12+02:00'),
+		celkTrzba: 3411300,
+		idProvoz: 273,
+		offline: false,
+	};
+
+	const options = {
+		playground: true,
+		privateKey: FAKE_PRIVATE_KEY,
+		certificate: CERTIFICATE,
+		offline: false,
+	};
+
+	t.plan(1);
+
+	return new EETClient(options).request(data)
+		.then(() => t.fail())
+		.catch(error => {
+			t.is(error._kod, '4');
+			t.log('Error:', error['#text']);
+		});
+
+});
+
+test('request wrong offline', async t => {
 
 	const data = {
 		prvniZaslani: true,
@@ -131,16 +214,45 @@ test('do request', async t => {
 
 	const options = {
 		playground: true,
+		privateKey: FAKE_PRIVATE_KEY,
+		certificate: CERTIFICATE,
+	};
+
+	t.plan(1);
+
+	return new EETClient(options).request(data)
+		.then(() => t.fail())
+		.catch(error => {
+			t.is(error._kod, '4');
+			t.log('Error:', error['#text']);
+		});
+
+});
+
+test('getWarnings wrong datTrzby', async t => {
+
+	const data = {
+		prvniZaslani: true,
+		overeni: false,
+		dicPopl: 'CZ1212121218',
+		idPokl: '/5546/RO24',
+		poradCis: '0/6460/ZQ42',
+		datTrzby: new Date('2000-01-01T00:30:12+02:00'),
+		celkTrzba: 3411300,
+		idProvoz: 273,
+	};
+
+	const options = {
+		playground: true,
 		privateKey: PRIVATE_KEY,
 		certificate: CERTIFICATE,
 	};
 
-	const { response: { fik } } = await new EETClient(options).request(data);
+	const { response: { fik, warnings } } = await new EETClient(options).request(data);
 
-	t.truthy(fik && fik.length === 39);
-
-	t.log('fik', fik);
-
-	// TODO test offline
+	t.not(fik, undefined);
+	t.not(warnings, undefined);
+	t.is(warnings._kod_varov, '5');
+	t.log('Warning:', warnings['#text']);
 
 });
