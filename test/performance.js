a5256a9cd1ffa35434c5e2f62ca181d9d0b57dab
Martin Endler <pokusew@seznam.cz>
2020-03-18 08:39:34 +0100

    Fix "performance" test

--- test/performance.js
+++ test/performance.js
@@ -1,18 +1,20 @@
 "use strict";
 
-const { EETClient } = require('../src/EETClient');
+const { randomBytes } = require('crypto');
 const fs = require('fs');
 
+const { eetSend } = require('../src/index');
 
-const PRIVATE_KEY = fs.readFileSync('./test/keys/private.pem');
-const CERTIFICATE = fs.readFileSync('./test/keys/certificate.pem');
+
+const PRIVATE_KEY = fs.readFileSync('./test/certificate-CZ1212121218/private.pem');
+const CERTIFICATE = fs.readFileSync('./test/certificate-CZ1212121218/certificate.pem');
 
 
 const options = {
 	privateKey: PRIVATE_KEY,
 	certificate: CERTIFICATE,
 	playground: true,
-	timeout: 2000,
+	timeout: 20000,
 	measureResponseTime: true,
 	offline: true,
 };
@@ -25,56 +27,58 @@ const items = {
 	overeni: false,
 
 	dicPopl: 'CZ1212121218',
-	idProvoz: '273',
-	idPokl: '/5546/RO24',
+	idProvoz: 273,
+	idPokl: '/554/RO24',
 
 	poradCis: '0/6460/ZQ42',
 	datTrzby: new Date(),
 
-	// celkTrzba: 1000.00,
-	// urcenoCerpZuct: 1000.00,
+	// celkTrzba: 100000, // 1000 CZK
+	// urcenoCerpZuct: 100000, // 1000 CZK
 
-	celkTrzba: 121.00,
-	zaklDan1: 100.00,
-	dan1: 21.00,
-	cerpZuct: 121.00,
+	celkTrzba: 12100, // 121 CZK
+	zaklDan1: 10000, // 100 CZK
+	dan1: 2100, // 21 CZK
+	cerpZuct: 12100, // 121 CZK
 
 	rezim: 0,
 
 };
 
-const TEST_CASES = 5;
+const TEST_CASES = 20;
 const start = process.hrtime();
 const timing = [];
 const test = [];
 
-const client = new EETClient(options);
-
 for (let i = 0; i < TEST_CASES; i++) {
 
 	const time = process.hrtime();
+	const id = randomBytes(5).toString('hex');
 
-	test.push(
-		client.request(items)
-			.then(response => {
+		test.push(
+			eetSend({
+				...items,
+				poradCis: `${id}-${i.toString()}`,
+			}, options)
+				.then(({ response, request }) => {
 
-				const diff = process.hrtime(time);
+					const diff = process.hrtime(time);
 
-				const elapsedMilliseconds = (diff[0] * 1000) + (diff[1] / 1000 / 1000);
+					const elapsedMilliseconds = (diff[0] * 1000) + (diff[1] / 1000 / 1000);
 
-				const processTime = Math.round(elapsedMilliseconds * 100) / 100 - response.duration;
+					const processTime = Math.round(elapsedMilliseconds * 100) / 100 - response.responseTime;
 
-				timing.push(processTime);
+					timing.push(processTime);
 
-				//console.log('Processed in', elapsedMilliseconds, response);
+					//console.log('Processed in', elapsedMilliseconds, response);
 
-			})
-			.catch(err => {
+				})
+				.catch(err => {
 
-				console.log(err);
+					console.log(err);
 
-			}),
-	);
+				}),
+		);
 
 }
 
